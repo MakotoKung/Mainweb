680377a9602c32f430491074ddce41396cdac420
MakotoKung <angleoftail@gmail.com>
2022-06-11 20:24:41 +0700

    ค่อยแก้ กดแล้วเด้ง
    ปกติมันต้องกดแล้วเด้งได้ แต่อันนี้มันทำไม่ได้ เพราะ เลย ต้องไปเรียน รู้ js เอง

---
+++ java.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector("#btn");
+let slidebar = document.querySelector(".slidebar");
+
+function click() {
+    slidebar.classList.add('active');
+}
+btn.onclick = click() 
+
+
+
+
