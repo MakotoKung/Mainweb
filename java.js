2b9108787b589e0115d4fa0a13285a51a1961b71
MakotoKung <angleoftail@gmail.com>
2022-06-15 20:51:04 +0700

    ได้แล้วโว้ยยยยยย

--- java.js
+++ java.js
@@ -1,5 +1,10 @@
 var open = document.getElementById('open');
 var start = document.getElementById('start');
+var test = document.getElementById('1');
+var test2 = document.getElementById('2');
+var test3 = document.getElementById('3');
+var test4 = document.getElementById('4');
+
 window.onload = function()
 {
    setTimeout(function(){
@@ -7,6 +12,17 @@ window.onload = function()
    },2000);
    setTimeout(function(){
     start.classList.add('start');
+   },4000);
+   setTimeout(function(){
+    start.classList.remove('op0');
+   },4000);
+   setTimeout(function(){
+    test2.classList.add('start');
+   },4000);
+   setTimeout(function(){
+    test3.classList.add('start');
+   },4000); setTimeout(function(){
+    test4.classList.add('start');
    },4000);
   
 
