0b3db38f9fcbb4fea2916316dc13afe9b2b150dd
MakotoKung <angleoftail@gmail.com>
2022-06-15 20:51:04 +0700

    ลอง

--- java.js
+++ java.js
@@ -1,5 +1,7 @@
 var open = document.getElementById('open');
 var start = document.getElementById('start');
+var test = document.getElementById('1');
+var test2 = document.getElementById('2');
 window.onload = function()
 {
    setTimeout(function(){
@@ -7,6 +9,11 @@ window.onload = function()
    },2000);
    setTimeout(function(){
     start.classList.add('start');
+   },4000);
+   setTimeout(function(){
+    test.classList.add('start');
+   },4000);setTimeout(function(){
+    test2.classList.add('start');
    },4000);
   
 
